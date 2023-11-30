91085eecfe1b4902c5a32dcf1f09f78bc673a449
Migel Oliver Salvador <125593527+Oliver-Salvador@users.noreply.github.com>
2023-11-30 10:45:10 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import RProjectsPage from './projects/ProjectsPage';
 import './App.css';
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
-  );
-}
+<div className="container">
+    <ProjectsPage />
+</div>
+};
 
 export default App;
